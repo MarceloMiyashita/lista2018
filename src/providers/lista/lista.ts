b157ca7f7410212d25f7100d9d9a9639d2da9b6a
viniciusperenha <vinicius.perenha@impactit.com.br>
2018-04-14 13:41:07 -0300

    first

---
+++ src/providers/lista/lista.ts
@@ -0,0 +1,23 @@
+import { Http } from "@angular/http";
+import { Injectable } from '@angular/core';
+
+/*
+  Generated class for the ListaProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class ListaProvider {
+
+  url = "https://raw.githubusercontent.com/munifgebara/reactnative/master/lista/lista.json";
+
+  constructor(public http: Http) {
+    console.log('Hello ListaProvider Provider');
+  }
+
+  buscaLista(){
+    return this.http.get(this.url);
+  }
+
+}
